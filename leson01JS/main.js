e745b275de7553a61e26575d1c02ffd90bdba986
NataKononets <kononets.n.g@gmail.com>
2025-09-24 12:10:59 +0200

    homeWork1JS

---
+++ leson01JS/main.js
@@ -0,0 +1,27 @@
+// Коментар до коду він не впливає на роботу
+// let badCode = 'bug'; Ctrl + / Cmnd + /
+
+// var apple = 'apple';
+// let orange = 'orange';
+// const KEYVALUE = 'secret key';
+
+// let keyvalue = 123;
+// let keyValue = 123;
+// let KeyValue = 123;
+// let key_Value = 123;
+
+// keyvalue = 321;
+
+// let userName = 'John';
+// let UserName = "Bob";
+// let userText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, tempora.';
+
+let userAgeValue = 25;  
+let userNameNickname = "Natalia";
+
+userAgeValue = 30;               
+userNameNickname = "Nata";       
+
+
+console.log("Возраст:", userAgeValue);
+console.log("Никнейм:", userNameNickname);
